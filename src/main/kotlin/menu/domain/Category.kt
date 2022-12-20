1e731105b0a2299a28f361067446ce18081c0bf9
dltkd1395 <dltkd13956@naver.com>
2022-12-20 15:12:53 +0900

    refactor(Category) : 카테고리 이름 추가

--- src/main/kotlin/menu/domain/Category.kt
+++ src/main/kotlin/menu/domain/Category.kt
@@ -2,12 +2,12 @@ package menu.domain
 
 import menu.utils.*
 
-enum class Category(val number: Int, val foods: List<String>) {
-    JAPANESE(1, JAPANESE_FOOD.split(", ")),
-    KOREAN(2, KOREAN_FOOD.split(", ")),
-    CHINESE(3, CHINESE_FOOD.split(", ")),
-    ASIAN(4, ASIAN_FOOD.split(", ")),
-    WESTERN(5, WESTERN_FOOD.split(", "));
+enum class Category(val number: Int, val value: String, val foods: List<String>) {
+    JAPANESE(1, "일식", JAPANESE_FOOD.split(", ")),
+    KOREAN(2, "한식", KOREAN_FOOD.split(", ")),
+    CHINESE(3, "중식", CHINESE_FOOD.split(", ")),
+    ASIAN(4, "아시안", ASIAN_FOOD.split(", ")),
+    WESTERN(5, "양식", WESTERN_FOOD.split(", "));
 
     companion object {
         fun of(otherNumbewr: Int): Category =
